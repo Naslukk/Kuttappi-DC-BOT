ec169221d7fbd1af9e1aad889c4fef703ca48d17
Naslu kk <naslukk@gmail.com>
2024-07-01 18:37:28 +0530

    Express added

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
+const express = require('express');
+
+
+const app = express();
+const port = 3000; 
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
